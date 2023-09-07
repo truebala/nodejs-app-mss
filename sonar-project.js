ab34945b348a56db9b01dc11df189d175983e1b8
truebala <131427463+truebala@users.noreply.github.com>
2023-09-07 17:09:18 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,19 +1,13 @@
-const sonarqubeScanner = require('sonarqube-scanner');
+# SonarQube server URL
+sonar.host.url=http://43.204.143.16:9000/
 
-sonarqubeScanner({
-  serverUrl: 'http://13.232.173.210:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_16247fd25fd0952e8b6f7149aa6ec718ca5d2f84',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
-}, () => {});
+# Project identification
+sonar.projectKey=my-nodejs-project
+sonar.projectName=My Node.js Project
+sonar.projectVersion=1.0.0
+
+# Path to your source code
+sonar.sources=.
+
+# Language
+sonar.language=js
